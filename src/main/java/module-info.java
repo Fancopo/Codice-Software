23b3a531eef3e56d7b72369c767ee2acc7034a4c
Jiaji Huang <jiajihuang@Jiajis-MacBook-Air.local>
2026-03-03 14:43:05 +0100

    Initial version

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,5 @@
-module it.polimi.ingsw.software {
     requires javafx.controls;
     requires javafx.fxml;
 
 
-    opens it.polimi.ingsw.software to javafx.fxml;
-    exports it.polimi.ingsw.software;
 }
