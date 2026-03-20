8536534a9127c8e3b8430b0acb9e6ee149426b19
Tao Chen <tao.chen@mail.polimi.it>
2026-03-20 19:46:58 +0100

    prova

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,5 +1,16 @@
+<<<<<<< HEAD
     requires javafx.controls;
     requires javafx.fxml;
 
 
+=======
+module it.polimi.ingsw.am22 {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires java.smartcardio;
+
+
+    opens it.polimi.ingsw.am22 to javafx.fxml;
+    exports it.polimi.ingsw.am22;
+>>>>>>> 7c921dd (second commit)
 }
