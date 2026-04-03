f2ca1e4f93bac53d4b61baa767b5225dd70e0c6e
CHRISTIAN <christian.fang@mail.polimi.it>
2026-04-03 11:38:55 +0200

    Second version of Building

--- main/java/il/polimi/ingsw/am22/Launcher.java
+++
@@ -1,9 +0,0 @@
-package il.polimi.ingsw.am22;
-
-import javafx.application.Application;
-
-public class Launcher {
-    public static void main(String[] args) {
-        Application.launch(HelloApplication.class, args);
-    }
-}
--- main/java/il/polimi/ingsw/am22/character/CharacterEffect.java
+++
@@ -1,8 +0,0 @@
-package il.polimi.ingse.character;
-
-import javafx.application.Application;
-
-public interface CharacterEffect {
-    void applyImmediateEffect(Player player, Tribe tribe);
-    void addCharacter(Player player, Tribe tribe);
-}
