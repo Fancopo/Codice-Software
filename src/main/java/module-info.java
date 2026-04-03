424f92bf31d1914125e1f4353eda8bbb39125e0f
iacopo fang <iacopo.fang@mail.polimi.it>
2026-04-03 13:14:07 +0200

    nuovo

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,6 @@
 module it.polimi.ingsw.am {
     requires javafx.controls;
     requires javafx.fxml;
-    requires com.fasterxml.jackson.databind;
 
 
     opens it.polimi.ingsw.am22 to javafx.fxml;
